e37a1a325288b1bb86ddfb4aaaccff68d0ae5246
Karennn-Y <113325061+Karennn-Y@users.noreply.github.com>
2024-04-30 18:13:00 +0900

    Setting

---
+++ src/main/java/com/example/recruitingproject/RecruitingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.recruitingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecruitingProjectApplication.class, args);
+    }
+
+}
